af28f8518ea7cad1fdc66af0870df9eb8feb7592
RitikDwivedi <ritikcu@gmail.com>
2024-06-20 15:13:16 +0530

    updated

--- Back-end/db/database.js
+++ Back-end/db/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 async function mongooseconnection(){
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGODBURL)
 }
 module.exports = mongooseconnection;
